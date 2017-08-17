f3d6e7eb447d9b2203f3281372a42dce55b3d33d
RPJpeg <ryanjones83@gmail.com>
2017-08-16 20:18:16 -0400

    Cleaned up the initial code
    Also, re-coded the nav to look a bit better -RJ

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-  $('#megaMenu').click(function() {
-  $('#megaCarot').toggleClass('rotated');
-  $(this).toggleClass('selected');
-});
+  $('html').click(function() {
+   $('#megaMenu').fadeOut( "fast",function() {
+   });
+   $('#megaCarot').removeClass("carot-rotated");
+   $('#nav-game-button').removeClass("menu-selected");
+  });
+
+  $('#megaWrap').click(function(event){
+       event.stopPropagation();
+  });
+
+  $( "#megaMenuTrig" ).click(function() {
+  $( "#megaMenu" ).fadeToggle( "fast", function() {
+    });
+  $('#megaCarot').toggleClass("carot-rotated");
+  $('#nav-game-button').toggleClass("menu-selected");
+  });
 
 });
