270826a738c1d93a7c02b86dc8fec7fd2d5e4c02
RPJpeg <ryanjones83@gmail.com>
2017-08-20 17:55:23 -0400

    Mobile Menu
    Finished Mobile Menu and some final touches on mega menu.  -RJ

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
   $('html').click(function() {
    $('#megaMenu').fadeOut( "fast",function() {
    });
+   $('#megaMobileMenu').slideUp( "fast",function() {
+   });
+   $('#scrollMobileMenu').slideUp( "fast",function() {
+   });
    $('#megaCarot').removeClass("carot-rotated");
    $('#nav-game-button').removeClass("menu-selected");
   });
 
   $('#megaWrap').click(function(event){
+       event.stopPropagation();
+  });
+  $('#megaMobileWrap').click(function(event){
        event.stopPropagation();
   });
 
@@ -17,5 +24,16 @@ $(document).ready(function() {
   $('#megaCarot').toggleClass("carot-rotated");
   $('#nav-game-button').toggleClass("menu-selected");
   });
+
+  $( "#megaMobileMenuTrig" ).click(function() {
+  $( "#megaMobileMenu" ).slideToggle( "fast", function() {
+    });
+  });
+
+  $( "#scrollMobileMenuTrig" ).click(function() {
+  $( "#scrollMobileMenu" ).slideToggle( "fast", function() {
+    });
+  });
+
 
 });
